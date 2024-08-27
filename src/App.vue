79aacfbb6ddf85f910101b1d058ca5aacf5d86d2
DeverDev-IMO <mking2499@hotmail.com>
2024-08-27 23:56:15 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/Navbar.vue";
+import Drawer from "./components/Drawer.vue";
+import Sidebar from "./components/Sidebar.vue";
+</script>
+
+<template>
+  <div class="grid gap-1 grid-cols-1 ">
+    <div>
+      <Sidebar />
+    </div>
+    <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+      <p class="text-2xl text-gray-400 dark:text-gray-500">text</p>
+    </div>
+  </div>
+
+
+</template>
+
+<!-- <style scoped></style> -->
